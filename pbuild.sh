395162707c97a9fe3aadcd981ac385dcc577f6dc
MeM <totoliqing@foxmail.com>
2026-04-19 00:07:15 +0800

    linux平台使用run后缀

--- pbuild.sh
+++ pbuild.sh
@@ -7,5 +7,5 @@ set -e
 #npm install -g esbuild
 esbuild $PATH_SHELL/src/index.js --bundle --minify --format=esm --external:qjs:* --outfile=$PATH_SHELL/build/bundle.js
 
-$PATH_SHELL/bin/qjs-linux-x86_64 --std -c $PATH_SHELL/build/bundle.js -o $PATH_SHELL/build/dtc --exe $PATH_SHELL/bin/qjs-linux-x86_64
+$PATH_SHELL/bin/qjs-linux-x86_64 --std -c $PATH_SHELL/build/bundle.js -o $PATH_SHELL/build/dtc.run --exe $PATH_SHELL/bin/qjs-linux-x86_64
 $PATH_SHELL/bin/qjs-linux-x86_64 --std -c $PATH_SHELL/build/bundle.js -o $PATH_SHELL/build/dtc.exe --exe $PATH_SHELL/bin/qjs-windows-x86_64.exe
